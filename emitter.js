8c09233fd37342cf6134fa0ccc2ba7666542b83c
IIaKyJIuH <38308131+IIaKyJIuH@users.noreply.github.com>
2018-11-05 20:13:45 +0500

    v3.1

--- emitter.js
+++ emitter.js
@@ -8,6 +8,29 @@ const isStar = false;
 
 let eventDispatcher = new Map();
 
+class Event {
+    constructor(context, handler) {
+        this.context = context;
+        this.handler = handler;
+    }
+}
+
+function getAllEvents(event) {
+    let events = [event];
+    if (!event.includes('.')) {
+        return events;
+    }
+    let current = event;
+    let lastDot = current.lastIndexOf('.');
+    while (lastDot !== -1) {
+        current = current.slice(0, lastDot);
+        events.push(current);
+        lastDot = current.lastIndexOf('.');
+    }
+
+    return events;
+}
+
 /**
  * Возвращает новый emitter
  * @returns {Object}
@@ -26,7 +49,7 @@ function getEmitter() {
             if (!eventDispatcher.has(event)) {
                 eventDispatcher.set(event, []);
             }
-            const onEvent = { person: context, action: handler };
+            const onEvent = new Event(context, handler);
             let eventArray = eventDispatcher.get(event);
             eventArray.push(onEvent);
 
@@ -45,7 +68,7 @@ function getEmitter() {
                 .forEach(filtered =>{
                     eventDispatcher.set(filtered, eventDispatcher
                         .get(filtered)
-                        .filter(x => x.person !== context));
+                        .filter(x => x.context !== context));
                 });
 
             return this;
@@ -61,7 +84,7 @@ function getEmitter() {
             for (const each of allEvents) {
                 if (eventDispatcher.has(each)) {
                     eventDispatcher.get(each).forEach(x => {
-                        x.action.call(x.person);
+                        x.handler.call(x.context);
                     });
                 }
             }
@@ -93,16 +116,6 @@ function getEmitter() {
             console.info(event, context, handler, frequency);
         }
     };
-}
-
-function getAllEvents(event) {
-    const events = [event];
-    if (!event.includes('.')) {
-        return events;
-    }
-    events.push(event.split('.')[0]);
-
-    return events;
 }
 
 module.exports = {
